3a87b291dcff2c60e3438df0275231a044fd0933
IHprogram <ihm20202020@gmail.com>
2021-05-17 14:24:36 +0900

    mapとfilterの使い方

--- src/App.js
+++ src/App.js
@@ -3,11 +3,39 @@ import './App.css';
 
 // ↓JSXを使う場合
 function App() {
-  const hoge = <strong>"こんちは！"</strong>;
+  const tasks = [
+    { id: 1, title: "one" },
+    { id: 2, title: "two" },
+    { id: 3, title: "three" },
+    { id: 4, title: "four" },
+    { id: 5, title: "five" },
+  ];
+  const num1 = tasks.map((val, index) => {
+    return <div key={index}>IDは{val.id}、TITLEは{val.title}</div>
+  });
+
+  const num2 = tasks.filter((val) => { return val.id > 2 }
+  ).map((val, index) => {
+    return <div key={index}>IDは{val.id}、TITLEは{val.title}</div>
+  });
+
+  console.log(num1);
+  console.log(num2);
+
+  // indexOfは、引数に入っている値が何番目かを返す → filter
+
+  const array10 = [1, 2, 3, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7]
+  const result10 = array10.filter((num, index, self) => {
+    return self.indexOf(num) === index
+  })
+  console.log(result10)
+
 
   return (
     <div className="App">
-      <h1 class="red">{hoge}</h1>
+      <div>{num1}</div>
+      <hr></hr>
+      <div>{num2}</div>
     </div>
   );
 }
