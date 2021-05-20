4c0dcfade1b890630098de35bb1d374de662125e
IHprogram <ihm20202020@gmail.com>
2021-05-20 11:19:35 +0900

    関数コンポーネントを用いたstateの変更

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from "react";
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      // クラスでは、コンストラクタ内で、this.stateの初期値をセット
-      count: 0
-    }
-  }
-  render() {
-    return (<React.Fragment>
-      <p>現在の数字は {this.state.count} です</p>
-      {/*ボタンをクリックした時に、this.setState()を呼ぶことでcountステート を更新 */}
-      <button onClick={() => this.setState({ count: this.state.count + 1 })}> +1
-</button>
-      <button onClick={() => this.setState({ count: this.state.count - 1 })}>
-        -1
-</button>
-      <button onClick={() => this.setState({ count: 0 })}>0</button>
-    </React.Fragment>)
-  }
+import React, { Component, useState } from "react";
+
+// const Child = () => {
+//   const val = "";
+//   const [moji, setMoji] = useState('文字です');
+//   return (
+//     <div>
+//       <p>{moji}</p>
+//       <input type="text" />
+//       <button onClick={(e) => e.target.previousElementSibling.value = val}>送信</button>
+//       <button onClick={() => console.log(val)}>確認</button>
+//     </div>
+//   )
+// }
+
+const App = () => {
+  // count という名前の state 変数を宣言、初期値 0 をセット
+  const [count, setCount] = useState(0)
+  return (
+    <div> <p>現在の数字は{count}です</p>
+      {/* setCount()は、countを更新するための関数。countを引数で受け取ること も出来る */}
+      <button onClick={() => setCount(prevState => prevState + 1)}> +1</button>
+      <button onClick={() => setCount(count - 1)}>- 1</button> <button onClick={() => setCount(0)}>0</button>
+      <button onClick={() => console.log(typeof setCount)}>useStateボタン</button>
+      {/* <Child /> */}
+    </div>)
 }
+
+
 export default App;
