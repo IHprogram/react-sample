529687852535d5c4c1ee288478ff9951d6b9f454
IHprogram <ihm20202020@gmail.com>
2021-05-20 15:38:30 +0900

    connectの設定

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,75 @@
 // 関数コンポーネントのuseEffect
 
-import React, { useState, useEffect } from 'react'
+import React, { Component, useState, useEffect } from 'react'
+import { connect } from 'react-redux';
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-    console.log("変更されてます")
-  }, [count])
+import { countPlus, countMinus } from '../actions'; // ActionsCreatorであるcountPlus、countMiusを読み込み
 
-  const kansu = () => {
-    setCount(0)
-    console.log("呼ばれてますよ")
+// クラスコンポーネントは以下の通り
+class App extends React.Component {
+  render() {
+    console.log('クラスコンポは以下の通り');
+    console.log(this); // thisにはAppクラスの中身が入っている
+    const props = this.props
+    return (
+      <React.Fragment>
+        <p>現在の数字は{props.val}です</p>
+        <button onClick={props.countPlus}>+1</button>
+        <button onClick={props.countMinus}>-1</button>
+      </React.Fragment>
+    )
   }
-
-  return (
-    <>
-      <p>{`${count}回クリックされました`}</p>
-      <div >
-        <button onClick={() => setCount((prev) => prev + 1)}>
-          ボタン
-        </button>
-        <button onClick={kansu}>
-          リセット
-        </button>
-      </div>
-    </>
-  )
 }
 
-export default App
+
+// 関数コンポーネントなら以下の通り
+// const App = (hoge) => {
+//   const props = hoge;
+//   return (
+//     <React.Fragment>
+//       <p>現在の数字は{props.val}です</p>
+//       <button onClick={props.countPlus}>+1</button>
+//       <button onClick={props.countMinus}>-1</button>
+//     </React.Fragment>
+//   )
+// }
+
+
+const mapStateToProps = state => ({
+  val: state.counter.val
+})
+
+const mapDispatchToProps = dispatch => ({
+  countPlus: () => dispatch(countPlus()),
+  countMinus: () => dispatch(countMinus())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// const App = () => {
+//   const [count, setCount] = useState(0)
+//   useEffect(() => {
+//     console.log("変更されてます")
+//   }, [count])
+
+//   const kansu = () => {
+//     setCount(0)
+//     console.log("呼ばれてますよ")
+//   }
+
+//   return (
+//     <>
+//       <p>{`${count}回クリックされました`}</p>
+//       <div >
+//         <button onClick={() => setCount((prev) => prev + 1)}>
+//           ボタン
+//         </button>
+//         <button onClick={kansu}>
+//           リセット
+//         </button>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
